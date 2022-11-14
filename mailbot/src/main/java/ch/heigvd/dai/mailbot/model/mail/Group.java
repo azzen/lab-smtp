3de482a90a5852412c6330a9ad5f806eb6abea8c
PabSaez <saez.pablo@outlook.com>
2022-11-14 16:54:49 +0100

    Implémentation Group, Message, Person

--- mailbot/src/main/java/ch/heigvd/dai/mailbot/model/mail/Group.java
+++ mailbot/src/main/java/ch/heigvd/dai/mailbot/model/mail/Group.java
@@ -1,4 +1,16 @@
 package ch.heigvd.dai.mailbot.model.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+    private final List<Person> members = new ArrayList<>();
+
+    public void addMember(Person person) {
+        members.add(person);
+    }
+
+    public List<Person> getMembers() {
+        return new ArrayList<>(members);
+    }
 }
--- mailbot/src/main/java/ch/heigvd/dai/mailbot/model/mail/Person.java
+++ mailbot/src/main/java/ch/heigvd/dai/mailbot/model/mail/Person.java
@@ -1,4 +1,27 @@
 package ch.heigvd.dai.mailbot.model.mail;
 
 public class Person {
+    private final String fname;
+    private final String lname;
+    private final String address;
+
+    Person (String fname, String lname, String address) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+    }
+
+    // TODO Autre constructeur ?
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
