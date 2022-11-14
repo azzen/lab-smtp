d6893bcadb17eddbfc061e5b67b9473c2cf97e10
PabSaez <saez.pablo@outlook.com>
2022-11-14 17:28:45 +0100

    update + add class Prank

--- mailbot/src/main/java/ch/heigvd/dai/mailbot/model/prank/Prank.java
+++ mailbot/src/main/java/ch/heigvd/dai/mailbot/model/prank/Prank.java
@@ -1,4 +1,75 @@
 package ch.heigvd.dai.mailbot.model.prank;
 
+import ch.heigvd.dai.mailbot.model.mail.Message;
+import ch.heigvd.dai.mailbot.model.mail.Person;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Prank {
+    private Person victimSender;
+    private final List<Person> victimRecipients = new ArrayList<>();
+    private final List<Person> witnessRecipients = new ArrayList<>();
+    private String message;
+
+
+
+    public void addVictimRecipients(List<Person> victims) {
+        victimRecipients.addAll(victims);
+    }
+
+    public void addWitnessRecipients(List<Person> witness) {
+        witnessRecipients.addAll(witness);
+    }
+
+    public Person getVictimSender() {
+        return victimSender;
+    }
+
+    public void setVictimSender(Person victimSender) {
+        this.victimSender = victimSender;
+    }
+
+    public List<Person> getVictimRecipients() {
+        return victimRecipients;
+    }
+
+    public List<Person> getWitnessRecipients() {
+        return witnessRecipients;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /*
+    public Message generateMailMessage() {
+        Message msg = new Message();
+
+        msg.setBody(this.message.getBody() + "\r\n" + victimSender.getFirstName());
+
+        String[] to = victimRecipients
+                .stream()
+                .map(p -> p.getAddress())
+                .collect(Collectors.toList())
+                .toArray(new String[]{});
+        msg.setTo(to);
+
+        String[] cc = witnessRecipients
+                .stream()
+                .map(p -> p.getAddress())
+                .collect(Collectors.toList())
+                .toArray(new String[]{});
+        msg.setCc(cc);
+
+        msg.setFrom(victimSender.getAddress());
+        msg.setSubject(this.message.getSubject());
+
+        return msg;
+    }
+     */
 }
