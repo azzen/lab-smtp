76f59f15d78ddec287231a2964a7a4d14519f17e
azzen <6373020+azzen@users.noreply.github.com>
2022-11-27 21:03:14 +0100

    feat: add main program

--- mailbot/src/main/java/ch/heigvd/dai/mailbot/App.java
+++ mailbot/src/main/java/ch/heigvd/dai/mailbot/App.java
@@ -1,13 +1,48 @@
 package ch.heigvd.dai.mailbot;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import ch.heigvd.dai.mailbot.config.ConfigurationManager;
+import ch.heigvd.dai.mailbot.config.IConfigurationManager;
+import ch.heigvd.dai.mailbot.model.prank.Prank;
+import ch.heigvd.dai.mailbot.model.prank.PrankGenerator;
+import ch.heigvd.dai.mailbot.smtp.ISmtpClient;
+import ch.heigvd.dai.mailbot.smtp.SmtpClient;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class App
 {
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        startPranksCampaign();
+    }
+
+    public static void startPranksCampaign() {
+        IConfigurationManager config = ConfigurationManager.getInstance();
+        List<Prank> pranks = null;
+
+        try {
+            pranks = (new PrankGenerator(config)).generatePranks();
+        } catch (Exception ex) {
+            LOG.severe(ex.getMessage());
+            return;
+        }
+
+        ISmtpClient client = new SmtpClient(config.getSmtpServerAddress(), config.getSmtpPort(), config);
+
+        LOG.info("Launching pranks!");
+
+        for (Prank p : pranks) {
+            try {
+                client.sendMessage(p.generateMailMessage());
+            } catch (IOException ex) {
+                LOG.severe(ex.getMessage());
+                return;
+            } catch (RuntimeException ex) {
+                LOG.severe(ex.getMessage());
+            }
+        }
     }
 }
